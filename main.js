9c6879b7ad63c1ea090a488f2aca1e7179ab30a7
MiTi041 <michaeltissen365@gmail.com>
2021-10-08 11:55:10 +0200

    d
    d

---
+++ main.js
@@ -0,0 +1,34 @@
+var objPeople = [
+    {
+       username: "daniel",
+       password: "lalaland"
+    },
+    {
+       username: "josel",
+       password: "letzer"
+    },
+    {
+       username: "andreas",
+       password: "radelman"
+    },
+    {
+       username: "michael",
+       password: "Altschutz04"
+    }
+ ]
+
+ function getInfo(){
+     var username = document.getElementById("username").value
+     var password = document.getElementById("password").value
+     for(i =0;i<objPeople.length; i++){
+        if(username==objPeople[i].username && password == objPeople[i].password){
+           window.location="http://culu.live/taschenrechner.html"
+           return
+        }
+        else{
+           
+        }
+     }alert("Falscher Benutzername oder falsches Passwort")
+     refresh
+
+ }
